c935cb51270f6e70cf28e9c760a3512794278586
AkhongLoiChin <chienhoanglqd@gmail.com>
2025-03-15 13:19:01 +0700

    setting

--- protein_cluster/settings.py
+++ protein_cluster/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-)2qwjvja*0b1zsobz79glp*-pvm--^uajz!fst=p(##=n5o2c8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
